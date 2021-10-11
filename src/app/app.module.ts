3dc86350c338aaf57ff91f6a4f16d8b6b3ffc8fe
adam <ratkovskya333@gmail.com>
2021-10-11 18:13:09 +0200

    vytvorenie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome/welcome-page/welcome-page.component';
+import { HeaderComponent } from './header/header/header.component';
+import { BottomComponent } from './bottom/bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    HeaderComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
