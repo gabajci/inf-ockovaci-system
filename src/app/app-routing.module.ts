2aa8dcd02f0847d4173eca9b2c707bd2294b6d0b
adam <ratkovskya333@gmail.com>
2021-10-13 14:04:38 +0200

    rozpracovanie hlavicky

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login/login.component';
+import { WelcomePageComponent } from './welcome/welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'app-welcome-page', pathMatch: 'full' },
+  {
+    path: 'app-welcome-page',
+    component: WelcomePageComponent
+  },
+  {
+    path: 'app-login',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome/welcome-page/welcome-page.component';
 import { HeaderComponent } from './header/header/header.component';
-import { BottomComponent } from './bottom/bottom/bottom.component';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
     HeaderComponent,
-    BottomComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
